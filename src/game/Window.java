c4a6720a22132d824026f69e66b36ad9cb9821a3
Tony Albert Lima <80111558+TonyALima@users.noreply.github.com>
2022-01-16 18:07:43 -0300

    cleaning code and changing panels layouts

--- src/game/Window.java
+++ src/game/Window.java
@@ -3,14 +3,17 @@ package game;
 import menu.Menu;
 
 import javax.swing.*;
+import javax.swing.plaf.DimensionUIResource;
 import java.awt.*;
+import java.util.Hashtable;
 
 public class Window implements Runnable {
 
     private JFrame frame;
 
-    private final Menu menuParts;
-    private final Thread windowThread;
+    private Menu menuParts;
+    private Hashtable<String, JPanel> panels;
+    private Thread windowThread;
     private static boolean play, menu, selectMultiplayer, options, keyMap;
 
     // Constructor
@@ -19,6 +22,7 @@ public class Window implements Runnable {
         this.windowThread.start();
 
         this.menuParts = new Menu(windowThread);
+        this.panels = this.menuParts.getPanels();
 
         initFrame();
     }
@@ -61,13 +65,13 @@ public class Window implements Runnable {
     // Methods
     private void initFrame() {
         frame = new JFrame("PONG");
-        frame.add(menuParts.getMULTIPLAYER_PANEL());
-        frame.setResizable(false);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
+        frame.setPreferredSize(new DimensionUIResource(300, 200));
+        frame.add(panels.get("multiplayer"));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.setIconImage(Toolkit.getDefaultToolkit().getImage("/Pong_Icon.png"));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
     }
 
     @Override
@@ -80,22 +84,32 @@ public class Window implements Runnable {
                     e.printStackTrace();
                 }
             }
-            Container currentIn = frame.getContentPane();
-            frame.remove(currentIn.getComponent(0));
+            JPanel[] panelsArray = panels.values().toArray(new JPanel[panels.size()]);
+            for (int i = 0; i < panels.size(); i++){
+                try {
+                    frame.remove(panelsArray[i]);
+                }catch (Exception e) {}
+            }
 
             if (play) {
                 Game game = new Game(menuParts.isMultiplayer(), menuParts.getDifficultyLevel());
                 game.getGameThread().start();
                 frame.add(game.getCanvas());
+                frame.setPreferredSize(new DimensionUIResource(640, 480));
                 game.getCanvas().requestFocus();
+                frame.setResizable(false);
             } else if (menu) {
-                frame.add(menuParts.getMENU_PANEL());
+                frame.add(panels.get("menu"));
+                frame.setPreferredSize(new DimensionUIResource(300, 550));
             } else if (options) {
-                frame.add(menuParts.getOPTIONS_PANEL());
+                frame.add(panels.get("options"));
+                frame.setPreferredSize(new DimensionUIResource(300, 450));
             }else if (selectMultiplayer){
-                frame.add(menuParts.getMULTIPLAYER_PANEL());
+                frame.add(panels.get("multiplayer"));
+                frame.setPreferredSize(new DimensionUIResource(300, 200));
             }else if (keyMap){
-                frame.add(menuParts.getCONTROLS_PANEL());
+                frame.add(panels.get("controls"));
+                frame.setPreferredSize(new DimensionUIResource(300, 500));
             }
             frame.pack();
             frame.setLocationRelativeTo(null);
--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -4,101 +4,83 @@ import game.Window;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Hashtable;
 
 public class Menu {
 
-    private final JButton SELECT_MULTIPLAYER_BUTTON;
-    private final JButton PLAY_BUTTON;
-    private final JButton OPTIONS;
-    private final JButton SINGLE_PLAYER_BUTTON;
-    private final JButton MULTIPLAYER_BUTTON;
-    private final JButton OK;
-
-    private final JPanel MENU_PANEL;
-    private final JPanel OPTIONS_PANEL;
-    private final JPanel MULTIPLAYER_PANEL;
-    private final JPanel CONTROLS_PANEL;
-
-    private final JLabel NAME_LABEL;
-    private final JLabel SELECT_LABEL;
-    private final JLabel KEY_INSTRUCT_LABEL;
-    private final JLabel KEY_MAP_LABEL;
-    private final JLabel PAUSE_LABEL;
-
-    private final JComboBox<String> DIFFICULTY;
-    private final Color BACK_BUTTONS_COLOR = new Color(100, 100, 100);
-    private final Color BACK_PANELS_COLOR = new Color(200, 200, 200);
-    private final Color FONT_COLOR = new Color(220, 220, 220);
-    private final Font FONT = new Font("Times", Font.BOLD, 15);
-    private final Thread windowThread;
+    private JPanel menu_panel;
+    private JPanel optionsPanel;
+    private JPanel multiplayerPanel;
+    private JPanel controlsPanel;
+    private Thread windowThread;
     private boolean multiplayer;
     private int difficultyLevel;
-    private final Keys keys;
 
     // Constructor
     public Menu(Thread windowThread) {
-        this.keys = new Keys();
-
-        this.SELECT_LABEL = new JLabel("Dificuldade");
-        this.NAME_LABEL = new JLabel("Bom dia");
-        this.KEY_INSTRUCT_LABEL = new JLabel("CONTROLES ");
-        this.PAUSE_LABEL = new JLabel("Pause   :   ESC");
-        this.KEY_MAP_LABEL = new JLabel();
-
-        this.SINGLE_PLAYER_BUTTON = new JButton("Single player");
-        this.MULTIPLAYER_BUTTON = new JButton("Multiplayer");
-        this.SELECT_MULTIPLAYER_BUTTON = new JButton("Numero de jogadores");
-        this.PLAY_BUTTON = new JButton("PLAY");
-        this.OPTIONS = new JButton("Opções");
-        this.OK = new JButton("OK");
-
-        this.DIFFICULTY = new JComboBox<>();
+        this.menu_panel = new JPanel();
+        this.optionsPanel = new JPanel();
+        this.multiplayerPanel = new JPanel();
+        this.controlsPanel = new JPanel();
         startComponents();
-        this.MENU_PANEL = new JPanel();
-        this.OPTIONS_PANEL = new JPanel();
-        this.MULTIPLAYER_PANEL = new JPanel();
-        this.CONTROLS_PANEL = new JPanel();
-        startPanels();
         this.windowThread = windowThread;
     }
 
     // Getters
-    public JPanel getMENU_PANEL() {
-        return MENU_PANEL;
-    }
-
-    public JPanel getOPTIONS_PANEL() {
-        return OPTIONS_PANEL;
-    }
-
     public boolean isMultiplayer() {
         return multiplayer;
-    }
-
-    public JPanel getMULTIPLAYER_PANEL() {
-        return MULTIPLAYER_PANEL;
     }
 
     public int getDifficultyLevel() {
         return difficultyLevel;
     }
 
-    public JPanel getCONTROLS_PANEL() {
-        return CONTROLS_PANEL;
-    }
+    public Hashtable<String, JPanel> getPanels() {
+        Hashtable<String, JPanel> panels = new Hashtable<String, JPanel>();
+        panels.put("menu", menu_panel);
+        panels.put("options", optionsPanel);
+        panels.put("multiplayer", multiplayerPanel);
+        panels.put("controls", controlsPanel);
+        return panels;
+    } 
 
     // Methods
     private void startComponents() {
-        PAUSE_LABEL.setFont(new Font("Times", Font.BOLD, 17));
-        NAME_LABEL.setFont(FONT);
-        SELECT_LABEL.setFont(FONT);
-        ImageIcon icon = new ImageIcon(keys.getKeys());
-        KEY_MAP_LABEL.setIcon(icon);
+        Color BACK_BUTTONS_COLOR = new Color(100, 100, 100);
+        Color BACK_PANELS_COLOR = new Color(200, 200, 200);
+        Color FONT_COLOR = new Color(220, 220, 220);
+        Font FONT = new Font("Times", Font.BOLD, 15);
+        
+        JLabel selectLabel = new JLabel("Dificuldade");
+        JLabel nameLabel = new JLabel("Bom dia");
+        JLabel keyInstructLabel = new JLabel("CONTROLES ");
+        JLabel pauseLabel = new JLabel("Pause   :   ESC");
+        JLabel keyMapLabel = new JLabel();
+        JComboBox<String> difficulty = new JComboBox<>();
+        Keys keys = new Keys();
+        JButton multiplayerButton = new JButton("Multiplayer");
+        JButton singlePlayerButton = new JButton("Single player");
+        JButton selectMultiplayerButton = new JButton("Numero de jogadores");
+        JButton playButton = new JButton("PLAY");
+        JButton optionsButton = new JButton("Opções");
+        JButton okButton1 = new JButton("OK");
+        JButton okButton2 = new JButton("OK");
 
-        MULTIPLAYER_BUTTON.setBackground(BACK_BUTTONS_COLOR);
-        MULTIPLAYER_BUTTON.setForeground(FONT_COLOR);
-        MULTIPLAYER_BUTTON.setFont(FONT);
-        MULTIPLAYER_BUTTON.addActionListener(e -> {
+        pauseLabel.setFont(new Font("Times", Font.BOLD, 17));
+        pauseLabel.setHorizontalAlignment(JLabel.CENTER);
+        nameLabel.setFont(FONT);
+        nameLabel.setHorizontalAlignment(JLabel.CENTER);
+        selectLabel.setFont(FONT);
+        selectLabel.setHorizontalAlignment(JLabel.CENTER);
+        ImageIcon icon = new ImageIcon(keys.getKeys());
+        keyMapLabel.setIcon(icon);
+        keyMapLabel.setHorizontalAlignment(JLabel.CENTER);
+        keyInstructLabel.setHorizontalAlignment(JLabel.CENTER);
+        
+        multiplayerButton.setBackground(BACK_BUTTONS_COLOR);
+        multiplayerButton.setForeground(FONT_COLOR);
+        multiplayerButton.setFont(FONT);
+        multiplayerButton.addActionListener(e -> {
             this.multiplayer = true;
             Window.setKeyMap();
             synchronized (windowThread) {
@@ -106,10 +88,10 @@ public class Menu {
             }
         });
 
-        SINGLE_PLAYER_BUTTON.setBackground(BACK_BUTTONS_COLOR);
-        SINGLE_PLAYER_BUTTON.setForeground(FONT_COLOR);
-        SINGLE_PLAYER_BUTTON.setFont(FONT);
-        SINGLE_PLAYER_BUTTON.addActionListener(e -> {
+        singlePlayerButton.setBackground(BACK_BUTTONS_COLOR);
+        singlePlayerButton.setForeground(FONT_COLOR);
+        singlePlayerButton.setFont(FONT);
+        singlePlayerButton.addActionListener(e -> {
             this.multiplayer = false;
             Window.setKeyMap();
             synchronized (windowThread) {
@@ -117,40 +99,50 @@ public class Menu {
             }
         }); // go to key map panel
 
-        SELECT_MULTIPLAYER_BUTTON.setBackground(BACK_BUTTONS_COLOR);
-        SELECT_MULTIPLAYER_BUTTON.setFont(FONT);
-        SELECT_MULTIPLAYER_BUTTON.setForeground(FONT_COLOR);
-        SELECT_MULTIPLAYER_BUTTON.addActionListener(e -> {
+        selectMultiplayerButton.setBackground(BACK_BUTTONS_COLOR);
+        selectMultiplayerButton.setFont(FONT);
+        selectMultiplayerButton.setForeground(FONT_COLOR);
+        selectMultiplayerButton.addActionListener(e -> {
             Window.setMultiplayer();
             synchronized (windowThread) {
                 windowThread.notify();
             }
         }); // go to selectPanel
 
-        PLAY_BUTTON.setBackground(BACK_BUTTONS_COLOR);
-        PLAY_BUTTON.setFont(FONT);
-        PLAY_BUTTON.setForeground(FONT_COLOR);
-        PLAY_BUTTON.addActionListener(e -> {
+        playButton.setBackground(BACK_BUTTONS_COLOR);
+        playButton.setFont(FONT);
+        playButton.setForeground(FONT_COLOR);
+        playButton.addActionListener(e -> {
             Window.setPlay();
             synchronized (windowThread) {
                 windowThread.notify();
             }
         }); // go to the game
 
-        OPTIONS.setBackground(BACK_BUTTONS_COLOR);
-        OPTIONS.setFont(FONT);
-        OPTIONS.setForeground(FONT_COLOR);
-        OPTIONS.addActionListener(e -> {
+        optionsButton.setBackground(BACK_BUTTONS_COLOR);
+        optionsButton.setFont(FONT);
+        optionsButton.setForeground(FONT_COLOR);
+        optionsButton.addActionListener(e -> {
             Window.setOptions();
             synchronized (windowThread) {
                 windowThread.notify();
             }
         }); // go to options panel
 
-        OK.setBackground(BACK_BUTTONS_COLOR);
-        OK.setForeground(FONT_COLOR);
-        OK.setFont(FONT);
-        OK.addActionListener(e -> {
+        okButton1.setBackground(BACK_BUTTONS_COLOR);
+        okButton1.setFont(FONT);
+        okButton1.setForeground(FONT_COLOR);
+        okButton1.addActionListener(e -> {
+            Window.setMenu();
+            synchronized (windowThread) {
+                windowThread.notify();
+            }
+        });
+
+        okButton2.setBackground(BACK_BUTTONS_COLOR);
+        okButton2.setFont(FONT);
+        okButton2.setForeground(FONT_COLOR);
+        okButton2.addActionListener(e -> {
             Window.setMenu();
             synchronized (windowThread) {
                 windowThread.notify();
@@ -159,112 +151,38 @@ public class Menu {
 
         String[] list = {"Easy", "Medium", "Hard", "God"};
 
-        DIFFICULTY.setFont(FONT);
-        DIFFICULTY.setModel(new DefaultComboBoxModel<>(list));
-        DIFFICULTY.addActionListener(e ->
-                difficultyLevel = DIFFICULTY.getSelectedIndex());
-    }
+        difficulty.setFont(FONT);
+        difficulty.setModel(new DefaultComboBoxModel<>(list));
+        difficulty.addActionListener(e ->
+                difficultyLevel = difficulty.getSelectedIndex());
 
-    private void startPanels() {
-        GroupLayout menuLayout = new GroupLayout(MENU_PANEL);
-        MENU_PANEL.setLayout(menuLayout);
-        menuLayout.setHorizontalGroup(
-                menuLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
-                        .addGap(20)
-                        .addComponent(NAME_LABEL)
-                        .addComponent(SELECT_MULTIPLAYER_BUTTON)
-                        .addComponent(OPTIONS)
-                        .addComponent(PLAY_BUTTON)
-                        .addGap(20)
-        );
-        menuLayout.setVerticalGroup(
-                menuLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
-                        .addGroup(menuLayout.createSequentialGroup()
-                                .addGap(20)
-                                .addComponent(NAME_LABEL)
-                                .addGap(10)
-                                .addComponent(SELECT_MULTIPLAYER_BUTTON)
-                                .addGap(10)
-                                .addComponent(OPTIONS)
-                                .addGap(10)
-                                .addComponent(PLAY_BUTTON)
-                                .addGap(20))
+        menu_panel.setLayout(new GridLayout(5, 1, 0, 10));
+        menu_panel.setBackground(BACK_PANELS_COLOR);
+        menu_panel.add(nameLabel);
+        menu_panel.add(selectMultiplayerButton);
+        menu_panel.add(optionsButton);
+        menu_panel.add(playButton);
+        menu_panel.setOpaque(true);
 
-        );
-        MENU_PANEL.setBackground(BACK_PANELS_COLOR);
+        optionsPanel.setLayout(new GridLayout(3, 1, 0, 10));
+        optionsPanel.setBackground(BACK_PANELS_COLOR);
+        optionsPanel.add(selectLabel);
+        optionsPanel.add(difficulty);
+        optionsPanel.add(okButton1);
+        optionsPanel.setOpaque(true);
 
-        GroupLayout selectLayout = new GroupLayout(OPTIONS_PANEL);
-        OPTIONS_PANEL.setLayout(selectLayout);
-        selectLayout.setHorizontalGroup(
-                selectLayout.createSequentialGroup()
-                        .addGap(10)
-                        .addGroup(
-                                selectLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
-                                        .addComponent(SELECT_LABEL)
-                                        .addComponent(DIFFICULTY)
-                                        .addComponent(OK))
-                        .addGap(10)
-        );
-        selectLayout.setVerticalGroup(
-                selectLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
-                        .addGroup(selectLayout.createSequentialGroup()
-                                .addGap(20)
-                                .addComponent(SELECT_LABEL)
-                                .addGap(10)
-                                .addComponent(DIFFICULTY)
-                                .addGap(10)
-                                .addComponent(OK)
-                                .addGap(20))
+        multiplayerPanel.setLayout(new GridLayout(2, 1, 0, 10));
+        multiplayerPanel.setBackground(BACK_PANELS_COLOR);
+        multiplayerPanel.add(multiplayerButton);
+        multiplayerPanel.add(singlePlayerButton);
+        multiplayerPanel.setOpaque(true);
 
-        );
-        OPTIONS_PANEL.setBackground(BACK_PANELS_COLOR);
-
-        GroupLayout multiplayerLayout = new GroupLayout(MULTIPLAYER_PANEL);
-        MULTIPLAYER_PANEL.setLayout(multiplayerLayout);
-        multiplayerLayout.setHorizontalGroup(
-                multiplayerLayout.createSequentialGroup()
-                        .addGap(10)
-                        .addGroup(
-                                multiplayerLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
-                                        .addComponent(MULTIPLAYER_BUTTON)
-                                        .addComponent(SINGLE_PLAYER_BUTTON))
-                        .addGap(10)
-        );
-        multiplayerLayout.setVerticalGroup(
-                multiplayerLayout.createSequentialGroup()
-                        .addGap(20)
-                        .addComponent(MULTIPLAYER_BUTTON)
-                        .addGap(10)
-                        .addComponent(SINGLE_PLAYER_BUTTON)
-                        .addGap(20)
-        );
-        MULTIPLAYER_PANEL.setBackground(BACK_PANELS_COLOR);
-
-        GroupLayout keyMapLayout = new GroupLayout(CONTROLS_PANEL);
-        CONTROLS_PANEL.setLayout(keyMapLayout);
-        keyMapLayout.setHorizontalGroup(
-                keyMapLayout.createSequentialGroup()
-                        .addGap(10)
-                        .addGroup(
-                                keyMapLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
-                                        .addComponent(KEY_INSTRUCT_LABEL)
-                                        .addComponent(KEY_MAP_LABEL)
-                                        .addComponent(PAUSE_LABEL)
-                                        .addComponent(OK)
-                        )
-                        .addGap(10)
-        );
-        keyMapLayout.setVerticalGroup(
-                keyMapLayout.createSequentialGroup()
-                        .addGap(10)
-                        .addComponent(KEY_INSTRUCT_LABEL)
-                        .addGap(10)
-                        .addComponent(KEY_MAP_LABEL)
-                        .addComponent(PAUSE_LABEL)
-                        .addGap(10)
-                        .addComponent(OK)
-                        .addGap(10)
-        );
-        CONTROLS_PANEL.setBackground(BACK_PANELS_COLOR);
+        controlsPanel.setLayout(new GridLayout(4, 1, 0, 10));
+        controlsPanel.setBackground(BACK_PANELS_COLOR);
+        controlsPanel.add(keyInstructLabel);
+        controlsPanel.add(keyMapLabel);
+        controlsPanel.add(pauseLabel);
+        controlsPanel.add(okButton2);
+        controlsPanel.setOpaque(true);
     }
 }
